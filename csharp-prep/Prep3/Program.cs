8a40060326c985a72e0eaec7f2e66e547dc3901d
bodilyr <bodilyr@yahoo.com>
2024-11-09 21:22:44 -0700

    Successful completion of C# Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        /*Console.Write("What is the magic number? ");
+        string userInput = Console.ReadLine();
+        int magicNumber = int.Parse(userInput);
+        */
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,101);
+        //Console.WriteLine ($"{magicNumber}");
+
+        int guessNumber = 0;
+        do
+        {
+            Console.Write("What is your guess? ");
+            string userGuess = Console.ReadLine();
+            guessNumber = int.Parse(userGuess);
+
+            if (guessNumber > magicNumber)
+            {
+                Console.WriteLine ("Lower");
+            }
+                else if (guessNumber < magicNumber)
+                {
+                    Console.WriteLine ("Higher");
+                }
+                else
+                {
+                    Console.WriteLine ("You guessed it correctly!");
+                }
+        }while (guessNumber != magicNumber);
+        
     }
 }
