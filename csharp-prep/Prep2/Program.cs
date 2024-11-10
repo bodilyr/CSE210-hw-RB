92e0e62fbd134443a322752d45f5a71e2d29ca1f
bodilyr <bodilyr@yahoo.com>
2024-11-09 20:03:57 -0700

    Successful completion of C# Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your grade percentage: ");
+        string gradeString = Console.ReadLine();
+        int gradePercent = int.Parse(gradeString);
+
+        string letter = "";
+    
+        if (gradePercent >= 90)
+        {
+            //Console.WriteLine ("Your grade is an A.");
+            letter = "A";
+        }
+            else if (gradePercent >= 80)
+            {
+                //Console.WriteLine ("Your grade is a B.");
+                letter = "B";
+            }
+            else if (gradePercent >= 70)
+            {
+            //Console.WriteLine ("Your grade is a C.");
+            letter = "C";
+            }
+            else if (gradePercent >= 60)
+            {
+            //Console.WriteLine ("Your grade is a D.");
+            letter = "D";
+            }
+            else
+            {
+                letter = "F";
+            }
+        
+        Console.WriteLine ($"Your grade in this course is: {letter}");
+
+        if (gradePercent >=70)
+        {
+            Console.WriteLine ("Congratulations!  You passed the class!");
+        }
+            else
+            {
+                Console.WriteLine ("Sorry you didn't pass the class.  Keep trying!");
+            }
     }
 }
